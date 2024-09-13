8e1d5ecb4ea913df0fddd2f67e44e4eacd1d17ab
unknown <luankevinfmb@gmail.com>
2024-09-13 04:41:02 -0400

    navbar/logout -> home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
             </ul>
             <div className="nav-login-cart">
                 {localStorage.getItem('auth-token') ?
-                    <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/')}}>Logout</button>
+                    <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('https://invsbl3.github.io/ecommerce-frontend/')}}>Logout</button>
                     :
                     <Link to='/login'><button>Login</button></Link>
                 }
